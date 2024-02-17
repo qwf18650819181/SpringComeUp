8dbaa1e028d4a10db04e03f57c92de64d21adf66
邱万烽 <qwf18650819181@163.com>
2024-02-17 14:29:59 +0800

    mock demo

--- src/test/java/com/comeup/springcomeup/SpringComeUpApplicationTests.java
+++ src/test/java/com/comeup/springcomeup/SpringComeUpApplicationTests.java
@@ -2,23 +2,35 @@ package com.comeup.springcomeup;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.comeup.springcomeup.infrashtructure.db.domain.ConfigDictionaryEntity;
 import com.comeup.springcomeup.infrashtructure.db.service.ConfigDictionaryService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class SpringComeUpApplicationTests {
 
+    @MockBean
+    private ConfigDictionaryService configDictionaryService;
 
-    @Autowired
-    ConfigDictionaryService configDictionaryService;
+    IPage page = new Page(1, 10);
+    @BeforeEach
+    void init() {
+
+        ConfigDictionaryEntity entity = new ConfigDictionaryEntity();
+        entity.setId(23);
+        page.setRecords(List.of(entity));
+        when(configDictionaryService.page(page)).thenReturn(page);
+    }
 
     @Test
     void contextLoads() {
-        IPage page = new Page(1, 10);
         List records = configDictionaryService.page(page).getRecords();
         System.out.println(records);
 
