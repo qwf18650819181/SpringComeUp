e8c5caad497efa62077de8f83f09554c4b5a5d26
邱万烽 <qwf18650819181@163.com>
2024-02-29 14:56:51 +0800

    spring doc

--- src/main/java/com/comeup/springcomeup/interfaceImpl/controller/DemoController.java
+++ src/main/java/com/comeup/springcomeup/interfaceImpl/controller/DemoController.java
@@ -1,6 +1,14 @@
 package com.comeup.springcomeup.interfaceImpl.controller;
 
+import com.comeup.springcomeup.interfaceImpl.dto.Book;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,12 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @description:
  */
 @RestController
+@Tag(name = "Demo API", description = "demo to spring doc")
 public class DemoController {
 
-    @GetMapping("/demo")
-    public String demo() {
+    @GetMapping("/demo/{name}")
+    @Operation(summary = "案例", description = "案例 action")
+//    @ApiResponse(responseCode = "200", description = "demo success")
+    public String demo(/*@Parameter(description = "request name")*/ @PathVariable String name) {
         return "demo";
     }
+
+    @PostMapping("/demo/book")
+    @Operation(summary = "book 案例", description = "book 案例 action")
+//    @ApiResponse(responseCode = "200", description = "demoBook success")
+    public Book demoBook(/*@io.swagger.v3.oas.annotations.parameters.RequestBody*/ @RequestBody Book request) {
+        return request;
+    }
+
+
 
 
 }
